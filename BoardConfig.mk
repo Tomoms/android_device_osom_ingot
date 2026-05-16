05acbd2f26d3e507aa11dd4cf7117d4e080025dd
Tommaso Fonda <fondatommaso2@gmail.com>
2026-05-16 11:44:08 +0200

    ingot: rcu cmdline options
    Change-Id: I338e9247cb36002936757fa5b008a5308a69a177

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@ TARGET_NO_BOOTLOADER := true
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += rcutree.android_enable_rcu_lazy rcu_nocbs=all
 BOARD_BOOTCONFIG += androidboot.hardware=qcom
 BOARD_BOOTCONFIG += androidboot.memcg=1
 BOARD_BOOTCONFIG += androidboot.usbcontroller=a600000.dwc3
